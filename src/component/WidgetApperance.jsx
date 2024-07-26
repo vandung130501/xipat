01db96dafc5c960e9f404896f5f50e9a0ea660a4
DungLe <dunglv@omegatheme.com>
2024-07-26 15:48:07 +0700

    [fix_polaris_task]: add collapse for card

--- src/component/WidgetApperance.jsx
+++ src/component/WidgetApperance.jsx
@@ -2,6 +2,7 @@
 import {
   Card,
   Checkbox,
+  Collapsible,
   Grid,
   Icon,
   Select,
@@ -17,185 +18,211 @@ import {
   OPTIONS,
   OPTION_DEFAULT,
 } from "../appConst";
-import { PaintBrushFlatIcon } from "@shopify/polaris-icons";
+import {
+  PaintBrushFlatIcon,
+  CaretDownIcon,
+  CaretUpIcon,
+} from "@shopify/polaris-icons";
 import { CustomLabel } from "./CustomLabel";
+import { useState } from "react";
 export const WidgetApperance = (props) => {
   const { widgetAppearance, setWidgetAppearance } = props;
+  const [openCollapse, setOpenCollapse] = useState(true);
   return (
     <Card background="bg-surface">
-      <div className="flex-start">
-        <Icon source={PaintBrushFlatIcon} tone="textCritical" />
-        <Text as="span" variant="headingSm" tone="critical">
-          Widget appearance
-        </Text>
+      <div
+        className="flex-between mb-20"
+        onClick={() => {
+          setOpenCollapse(!openCollapse);
+        }}
+      >
+        <div className="flex-start">
+          <Icon source={PaintBrushFlatIcon} tone="textCritical" />
+          <Text as="span" variant="headingSm" tone="critical">
+            Widget appearance
+          </Text>
+        </div>
+        <div>
+          <Icon
+            source={!openCollapse ? CaretDownIcon : CaretUpIcon}
+            tone="textCritical"
+          />
+        </div>
       </div>
-      <Grid>
-        <Grid.Cell columnSpan={GRID_TWO_COLUMNS}>
-          <Select
-            label={
-              <CustomLabel
-                title="Layout"
-                titleColor={widgetAppearance?.titleColor}
-              />
-            }
-            options={OPTION_DEFAULT}
-            onChange={(value) =>
-              handleChangeValue("selected", value, setWidgetAppearance)
-            }
-            value={widgetAppearance.selected}
-          />
-        </Grid.Cell>
-        <Grid.Cell columnSpan={GRID_TWO_COLUMNS}>
-          <Select
-            label={
-              <CustomLabel
-                title="Calendar layout"
-                titleColor={widgetAppearance?.titleColor}
-              />
-            }
-            options={OPTIONS}
-            onChange={(value) =>
-              handleChangeValue("selected", value, setWidgetAppearance)
-            }
-            value={widgetAppearance.selected}
-          />
-          <Checkbox
-            label={
-              <CustomLabel
-                title="Always open the calendar"
-                titleColor={widgetAppearance?.titleColor}
-              />
-            }
-            checked={widgetAppearance.isAlwaysOpen}
-            onChange={(event) =>
-              handleChangeValue("isAlwaysOpen", event, setWidgetAppearance)
-            }
-          />
-        </Grid.Cell>
-        <Grid.Cell columnSpan={GRID_TWO_COLUMNS}>
-          <Select
-            label={
-              <CustomLabel
-                title="Calendar language"
-                titleColor={widgetAppearance?.titleColor}
-              />
-            }
-            options={LANGUGES}
-            onChange={(value) =>
-              handleChangeValue("language", value, setWidgetAppearance)
-            }
-            value={widgetAppearance.language}
-          />
-        </Grid.Cell>
-        <Grid.Cell columnSpan={GRID_TWO_COLUMNS}>
-          <Select
-            label={
-              <CustomLabel
-                title="First day of calendar"
-                titleColor={widgetAppearance?.titleColor}
-              />
-            }
-            options={DAYS_OF_WEEKS}
-            onChange={(value) =>
-              handleChangeValue("dayOfWeek", value, setWidgetAppearance)
-            }
-            value={widgetAppearance.dayOfWeek}
-          />
-        </Grid.Cell>
-        <Grid.Cell columnSpan={GRID_TWO_COLUMNS}>
-          <Select
-            label={
-              <CustomLabel
-                title="Date format"
-                titleColor={widgetAppearance?.titleColor}
-              />
-            }
-            options={DATE_FORMAT_OPTIONS}
-            onChange={(value) =>
-              handleChangeValue("dateFormat", value, setWidgetAppearance)
-            }
-            value={widgetAppearance.dateFormat}
-          />
-        </Grid.Cell>
-        <Grid.Cell columnSpan={GRID_TWO_COLUMNS}>
-          <div className="d-flex">
-            <TextField
+      <Collapsible
+        open={openCollapse}
+        id="basic-collapsible"
+        transition={{ duration: "500ms", timingFunction: "ease-in-out" }}
+        expandOnPrint
+      >
+        <Grid>
+          <Grid.Cell columnSpan={GRID_TWO_COLUMNS}>
+            <Select
               label={
                 <CustomLabel
-                  title="Theme color"
+                  title="Layout"
                   titleColor={widgetAppearance?.titleColor}
                 />
               }
-              value={widgetAppearance.themeColor}
-              type="text"
-            />
-            <input
-              type="color"
-              className="color-picker"
-              value={widgetAppearance.themeColor}
-              onChange={(event) =>
-                handleChangeValue(
-                  "themeColor",
-                  event.target.value,
-                  setWidgetAppearance
-                )
+              options={OPTION_DEFAULT}
+              onChange={(value) =>
+                handleChangeValue("selected", value, setWidgetAppearance)
               }
+              value={widgetAppearance.selected}
             />
-          </div>
-        </Grid.Cell>
-        <Grid.Cell columnSpan={GRID_TWO_COLUMNS}>
-          <div className="d-flex">
-            <TextField
+          </Grid.Cell>
+          <Grid.Cell columnSpan={GRID_TWO_COLUMNS}>
+            <Select
               label={
                 <CustomLabel
-                  title="Title color"
+                  title="Calendar layout"
                   titleColor={widgetAppearance?.titleColor}
                 />
               }
-              value={widgetAppearance.titleColor}
-              type="text"
-            />
-            <input
-              type="color"
-              className="color-picker"
-              value={widgetAppearance.titleColor}
-              onChange={(event) =>
-                handleChangeValue(
-                  "titleColor",
-                  event.target.value,
-                  setWidgetAppearance
-                )
+              options={OPTIONS}
+              onChange={(value) =>
+                handleChangeValue("selected", value, setWidgetAppearance)
               }
+              value={widgetAppearance.selected}
             />
-          </div>
-        </Grid.Cell>
-        <Grid.Cell columnSpan={GRID_TWO_COLUMNS}>
-          <div className="d-flex">
-            <TextField
-              value={widgetAppearance.requiredMessageColor}
+            <Checkbox
               label={
                 <CustomLabel
-                  title="Required message text color"
+                  title="Always open the calendar"
                   titleColor={widgetAppearance?.titleColor}
                 />
               }
-              type="text"
-            />
-            <input
-              type="color"
-              className="color-picker"
-              value={widgetAppearance.requiredMessageColor}
+              checked={widgetAppearance.isAlwaysOpen}
               onChange={(event) =>
-                handleChangeValue(
-                  "requiredMessageColor",
-                  event.target.value,
-                  setWidgetAppearance
-                )
+                handleChangeValue("isAlwaysOpen", event, setWidgetAppearance)
               }
             />
-          </div>
-        </Grid.Cell>
-      </Grid>
+          </Grid.Cell>
+          <Grid.Cell columnSpan={GRID_TWO_COLUMNS}>
+            <Select
+              label={
+                <CustomLabel
+                  title="Calendar language"
+                  titleColor={widgetAppearance?.titleColor}
+                />
+              }
+              options={LANGUGES}
+              onChange={(value) =>
+                handleChangeValue("language", value, setWidgetAppearance)
+              }
+              value={widgetAppearance.language}
+            />
+          </Grid.Cell>
+          <Grid.Cell columnSpan={GRID_TWO_COLUMNS}>
+            <Select
+              label={
+                <CustomLabel
+                  title="First day of calendar"
+                  titleColor={widgetAppearance?.titleColor}
+                />
+              }
+              options={DAYS_OF_WEEKS}
+              onChange={(value) =>
+                handleChangeValue("dayOfWeek", value, setWidgetAppearance)
+              }
+              value={widgetAppearance.dayOfWeek}
+            />
+          </Grid.Cell>
+          <Grid.Cell columnSpan={GRID_TWO_COLUMNS}>
+            <Select
+              label={
+                <CustomLabel
+                  title="Date format"
+                  titleColor={widgetAppearance?.titleColor}
+                />
+              }
+              options={DATE_FORMAT_OPTIONS}
+              onChange={(value) =>
+                handleChangeValue("dateFormat", value, setWidgetAppearance)
+              }
+              value={widgetAppearance.dateFormat}
+            />
+          </Grid.Cell>
+          <Grid.Cell columnSpan={GRID_TWO_COLUMNS}>
+            <div className="d-flex">
+              <TextField
+                label={
+                  <CustomLabel
+                    title="Theme color"
+                    titleColor={widgetAppearance?.titleColor}
+                  />
+                }
+                value={widgetAppearance.themeColor}
+                type="text"
+              />
+              <input
+                type="color"
+                className="color-picker"
+                value={widgetAppearance.themeColor}
+                onChange={(event) =>
+                  handleChangeValue(
+                    "themeColor",
+                    event.target.value,
+                    setWidgetAppearance
+                  )
+                }
+              />
+            </div>
+          </Grid.Cell>
+          <Grid.Cell columnSpan={GRID_TWO_COLUMNS}>
+            <div className="d-flex">
+              <TextField
+                label={
+                  <CustomLabel
+                    title="Title color"
+                    titleColor={widgetAppearance?.titleColor}
+                  />
+                }
+                value={widgetAppearance.titleColor}
+                type="text"
+              />
+              <input
+                type="color"
+                className="color-picker"
+                value={widgetAppearance.titleColor}
+                onChange={(event) =>
+                  handleChangeValue(
+                    "titleColor",
+                    event.target.value,
+                    setWidgetAppearance
+                  )
+                }
+              />
+            </div>
+          </Grid.Cell>
+          <Grid.Cell columnSpan={GRID_TWO_COLUMNS}>
+            <div className="d-flex">
+              <TextField
+                value={widgetAppearance.requiredMessageColor}
+                label={
+                  <CustomLabel
+                    title="Required message text color"
+                    titleColor={widgetAppearance?.titleColor}
+                  />
+                }
+                type="text"
+              />
+              <input
+                type="color"
+                className="color-picker"
+                value={widgetAppearance.requiredMessageColor}
+                onChange={(event) =>
+                  handleChangeValue(
+                    "requiredMessageColor",
+                    event.target.value,
+                    setWidgetAppearance
+                  )
+                }
+              />
+            </div>
+          </Grid.Cell>
+        </Grid>
+      </Collapsible>
     </Card>
   );
 };
